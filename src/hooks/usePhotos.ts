f752f7b661a1cf5a3f7248cfda29a70bdf232bed
ocheezyyy <seanodonnellse@gmail.com>
2025-03-06 15:19:33 -0500

    Typo fix on function name.

--- src/hooks/usePhotos.ts
+++ src/hooks/usePhotos.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchPhotos } from "@/api";
 import { Photo } from "@/schemas";
 
-export const useAlbums = () => {
+export const usePhotos = () => {
   return useQuery<Photo[], Error>({
     queryKey: ["photos"],
     queryFn: fetchPhotos,
